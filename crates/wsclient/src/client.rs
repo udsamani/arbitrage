d2c810f6ee680102cf5d43689004958731195864
udsamani <uditpsamani@hotmail.com>
2025-02-02 11:15:38 +0000

    feat: add missing writing messages via wsclient

--- crates/wsclient/src/client.rs
+++ crates/wsclient/src/client.rs
@@ -1,21 +1,43 @@
-use common::SharedRef;
+use common::{ArbitrageError, ArbitrageResult, Backoff, Context, SharedRef};
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio_tungstenite::tungstenite::Message;
+
+use crate::{WsCallback, WsConsumer};
 
 #[allow(unused)]
-#[derive(Clone)]
 pub struct WsClient {
     ws_url: String,
     connected: SharedRef<bool>,
+    sender: Sender<Message>,
     client_id: String,
     heartbeat_millis: u64,
+    _receiver: Option<Receiver<Message>>,
+}
+
+
+impl Clone for WsClient {
+    fn clone(&self) -> Self {
+        Self {
+            ws_url: self.ws_url.clone(),
+            connected: self.connected.clone(),
+            sender: self.sender.clone(),
+            client_id: self.client_id.clone(),
+            heartbeat_millis: self.heartbeat_millis,
+            _receiver: None,
+        }
+    }
 }
 
 impl WsClient {
     pub fn new(ws_url: String, heartbeat_millis: u64) -> Self {
+        let (sender, receiver) = tokio::sync::mpsc::channel(100);
         Self {
             ws_url,
             connected: SharedRef::new(false),
+            sender,
             client_id: "".to_string(),
             heartbeat_millis,
+            _receiver: Some(receiver),
         }
     }
 
@@ -30,5 +52,34 @@ impl WsClient {
 
     pub fn is_connected(&self) -> bool {
         *self.connected.lock()
+    }
+
+    pub fn write(&self, message: Message) -> ArbitrageResult<()> {
+        match self.sender.try_send(message) {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                Err(ArbitrageError::GenericError(format!("failed to send message to ws client: {}", e)))
+            }
+        }
+    }
+
+    pub fn close(&self) -> ArbitrageResult<()> {
+        self.write(Message::Close(None))
+    }
+
+    pub fn consumer<C>(&mut self, context: Context, callback: C) -> WsConsumer<C>
+    where
+        C: WsCallback,
+    {
+        let receiver = self._receiver.take().unwrap();
+        WsConsumer {
+            client_id: self.client_id.clone(),
+            ws_url: self.ws_url.clone(),
+            callback,
+            heartbeat_millis: self.heartbeat_millis,
+            backoff: Backoff::default(),
+            context,
+            receiver: Some(receiver),
+        }
     }
 }
